c4236fbb7a826ae7c22563cabf9bde0272601054
FabioMusi04 <76118020+FabioMusi04@users.noreply.github.com>
2025-08-13 11:14:03 +0200

    fix(auditor)

--- src/main/java/me/demo/auditable/AuditAware.java
+++ src/main/java/me/demo/auditable/AuditAware.java
@@ -6,11 +6,13 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import org.springframework.lang.NonNull;
 import me.demo.user.User;
 
 public class AuditAware implements AuditorAware<Long>{
 
   @Override
+  @NonNull
   public Optional<Long> getCurrentAuditor() {
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
     if (authentication == null || !authentication.isAuthenticated()) {
